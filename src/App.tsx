fd7684587b5066401817b627c30199f194af3600
yoshikizh <177365340@qq.com>
2023-07-23 21:26:12 +0800

    initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
+import { Button } from 'antd';
+import { ConfigProvider } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App: React.FC = () => (
+
+  <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
-  );
-}
+  </ConfigProvider>
+);
 
 export default App;
